02821e22bd415db72ef6faa8205f0ecd21ce0d3c
Guilherme Larré <guilhermelarre@gmail.com>
2025-06-11 11:36:18 -0300

    chore: mudar opções dos menus de letras para números

--- workspace_upe_programacao_2/upe_programacao_2/src/upe_programacao_2/Main.java
+++ workspace_upe_programacao_2/upe_programacao_2/src/upe_programacao_2/Main.java
@@ -12,25 +12,26 @@ public class Main {
 		JsonReader.carregarEstoque();
 		JsonReader.carregarHistorico();
 		
-		// Iniciar menu
+		// Iniciar loop do menu principal
 		Scanner sc = new Scanner(System.in);
 		loopMain: while (true) {
 			System.out.println(Menu.menuPrincipal);
 			String opcao = sc.nextLine();
 			switch (opcao) {
-				case "c":
+				// Menu clientes
+				case "1":
 					loopCliente: while (true) {
 						System.out.println(Menu.menuClientes);
 						opcao = sc.nextLine();
 						switch (opcao) {
-						case "e":
+						case "1":
 							System.out.println(Cliente.getClientes());
 							break;
-						case "h":
+						case "2":
 							System.out.println("Digite o id do Cliente: ");
 							int idCliente = sc.nextInt();
 							System.out.println(Historico.getHistoricoCliente(idCliente));
-						case "r":
+						case "3":
 							System.out.println("Digite o nome do cliente: ");
 							String nome = sc.nextLine();
 							System.out.println("Digite o cpf do cliente: ");
@@ -50,23 +51,23 @@ public class Main {
 							new Cliente(nome, cpf, telefone, email, preferenciaComunicacao, endereco, aniversario, genero);
 							System.out.println("Cliente registrado com sucesso!");
 							break;
-						case "s":
+						case "4":
 							break loopCliente;
 						}
 					}
-				case "f":
+				case "2":
 					loopFuncionario: while (true) {
 						System.out.println(Menu.menuFuncionarios);
 						opcao = sc.nextLine();
 						switch (opcao) {
-						case "e":
+						case "1":
 							System.out.println(Funcionario.getFuncionarios());
 							break;
-						case "h":
+						case "2":
 							System.out.println("Digite o id do Funcionário: ");
 							int idFuncionario = sc.nextInt();
 							System.out.println(Historico.getHistoricoFuncionario(idFuncionario));
-						case "r":
+						case "3":
 							System.out.println("Digite o nome do funcionário: ");
 							String nome = sc.nextLine();
 							System.out.println("Digite o cpf do funcionário: ");
@@ -88,32 +89,36 @@ public class Main {
 							new Funcionario(nome, cpf, telefone, email, preferenciaComunicacao, endereco, aniversario, genero, idCargo, salario);
 							System.out.println("Funcionário registrado com sucesso!");
 							break;
-						case "s":
+						case "4":
 							break loopFuncionario;
 						}
 					}
 				// TO DO: resolver parte de estoque
-				case "e":
+				case "3":
 					loopEstoque: while (true) {
 						System.out.println(Menu.menuEstoque);
 						opcao = sc.nextLine();
 						switch (opcao) {
-							case "m":
+							case "1":
+								
+								break;
+							case "2":
+								break;
+							case "3":
 								for (Produto produto : Produto.getListaProdutos()) {
 									System.out.println(produto);
 								}
 								break;
-							case "p":
-								
+							case "4":
 								break;
-							case "g":
-								break;
-							case "s":
+							case "5":
 								break loopEstoque;
 						}
 					}
-				case "s":
-					// TO DO: Salvar em json
+				case "4":
+					break;
+				case "5":
+					// TO DO: Salvar tudo em json antes de fechar
 					System.out.println("Até logo!");
 					sc.close();
 					break loopMain;
--- workspace_upe_programacao_2/upe_programacao_2/src/upe_programacao_2/Menu.java
+++ workspace_upe_programacao_2/upe_programacao_2/src/upe_programacao_2/Menu.java
@@ -7,54 +7,54 @@ public class Menu {
 
 Selecione a opção desejada:
 
-[c] = Clientes
-[f] = Funcionários
-[e] = Estoque
-[v] = Vendas
-[s] = Sair
+[1] = Clientes
+[2] = Funcionários
+[3] = Estoque
+[4] = Vendas
+[5] = Sair
 
 -> """;
 	
 	public static String menuClientes = """
 
-[e] = Exibir clientes
-[h] = Exibir histórico de compras de um cliente
-[r] = Registrar cliente
-[s] = Sair e retornar ao menu principal
+[1] = Exibir clientes
+[2] = Exibir histórico de compras de um cliente
+[3] = Registrar cliente
+[4] = Sair e retornar ao menu principal
 
 -> """;
 	
 	public static String menuFuncionarios = """
 
-[e] = Exibir funcionários
-[h] = Exibir histórico de vendas de um funcionário
-[r] = Registrar funcionário
-[s] = Sair e retornar ao menu principal
+[1] = Exibir funcionários
+[2] = Exibir histórico de vendas de um funcionário
+[3] = Registrar funcionário
+[4] = Sair e retornar ao menu principal
 
 -> """;
 	
 	public static String menuEstoque = """
 
-[m] = Exibir todo o estoque
-[c] = Exibir categoria de produtos
-[p] = Pesquisar produto em estoque
-[g] = Gerenciar estoque
-[s] = Sair e retornar ao menu principal
+[1] = Pesquisar um produto em estoque
+[2] = Exibir uma categoria de produtos
+[3] = Exibir todo o estoque
+[4] = Gerenciar estoque
+[5] = Sair e retornar ao menu principal
 
 -> """;
 	
 	public static String menuPesquisaEstoque = """
 
-[i] = Pesquisar por id do produto
-[n] = Pesquisar pelo nome do produto
+[1] = Pesquisar por id do produto
+[2] = Pesquisar pelo nome do produto
 
 """;
 	
 	public static String menuGerenciarEstoque = """
 
-[a] = Adicionar produto ao estoque
-[e] = Excluir produto do estoque
-[m] = Modificar produto no estoque
+[1] = Adicionar produto ao estoque
+[2] = Excluir produto do estoque
+[3] = Modificar produto no estoque
 
 """;
 	
@@ -62,12 +62,12 @@ Selecione a opção desejada:
 
 O que deseja alterar?
 
-[s] = SKU
-[n] = Nome do produto
-[d] = Descrição
-[v] = Valor
-[c] = Categoria
-[q] = Quantidade em estoque
+[1] = SKU
+[2] = Nome do produto
+[3] = Descrição
+[4] = Valor
+[5] = Categoria
+[6] = Quantidade em estoque
 
 """;
 	
